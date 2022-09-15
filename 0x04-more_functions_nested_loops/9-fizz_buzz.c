f033539c02631d8d300642eb1dfdd762d16b5130
Rachael Ijeoma <ijeomarachel01@gmail.com>
2022-09-15 14:49:33 +0100

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,7 +4,7 @@
 /**
  * main - prints the numbers from 1 to 100, prints Fizz for multiples of 3, prints Buzz for multiples of 5 and, FizzBuzz for multiples of both 3 and 5
  *
- * Return: 0
+ * Return: a number
  */
 int main(void)
 {
