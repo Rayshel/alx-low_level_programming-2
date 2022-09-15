2a1fd2bb11f7bfb19aea71cf8d8bde42ddbcf974
Rachael Ijeoma <ijeomarachel01@gmail.com>
2022-09-15 14:16:21 +0100

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character should be printed
+ *
+ */
+void print_line(int n)
+{
+	int a;
+
+	for (a = 0; a < n; a++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
